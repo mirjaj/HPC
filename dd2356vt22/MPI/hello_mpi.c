9d6f46af46f5a0d361c065bb3b7f5ab2a6cf7c31
mirjaj <mirjajohnsson@gmail.com>
2022-08-31 17:14:16 +0200

    Update hello_mpi.c

--- dd2356vt22/MPI/hello_mpi.c
+++ dd2356vt22/MPI/hello_mpi.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <mpi.h>
 
+int main(int argc, char const *argv[])
+{
+	int rank, size, i,provided;
+
+	MPI_Init_thread(&argc, &argv, MPI_THREAD_SINGLE, &provided);
+
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	printf("Hello from thread number %d  \n", rank);
+
+	MPI_Finalize();
+
+	return 0;
+}
